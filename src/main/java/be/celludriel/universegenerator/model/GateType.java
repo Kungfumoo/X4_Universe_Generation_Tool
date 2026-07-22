9347b0671c947e44f7f164213a5dd0b982122e04
sudo1701 <sudo1701@gmail.com>
2026-07-22 01:29:41 +0100

    Add new terran gate type

--- src/main/java/be/celludriel/universegenerator/model/GateType.java
+++ src/main/java/be/celludriel/universegenerator/model/GateType.java
@@ -4,7 +4,8 @@ public enum GateType {
     ACCELERATOR_01("props_gates_orb_accelerator_01_macro"),
     ACCELERATOR_02("props_gates_orb_accelerator_02_macro"),
     ANCIENT_GATE("props_gates_anc_gate_macro"),
-    TERRAN_GATE("props_ter_gate_01_macro");
+    TERRAN_GATE("props_ter_gate_01_macro"),
+    TERRAN_GATE_2("props_ter_gate_02_macro");
 
     private String macro;
 
